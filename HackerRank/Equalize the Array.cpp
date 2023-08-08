7bec35ca7c3b0b85f889a03e5304339d332746ed
Shipra Sinha <141357738+sshipra12@users.noreply.github.com>
2023-08-08 22:21:11 +0530

    Create Equalize the Array.cpp

---
+++ HackerRank/Equalize the Array.cpp
@@ -0,0 +1,28 @@
+/*
+ * Complete the 'equalizeArray' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+
+Q => Given an array of integers, determine the minimum number of elements to delete to leave only elements of equal value.
+ 
+ */
+
+int equalizeArray(vector<int> arr) {
+    int i,frequencyarray[101],maxfrequency=0;
+    for(i=0;i<101;i++){
+        frequencyarray[i]=0;
+    }
+    
+    for(i=0;i<arr.size();i++){
+        frequencyarray[arr[i]]=frequencyarray[arr[i]]+1;
+    }
+    
+    for(i=0;i<101;i++){
+        if(frequencyarray[i]>maxfrequency){
+            maxfrequency=frequencyarray[i];
+        }
+    }
+    
+    return arr.size()-maxfrequency;
+}
