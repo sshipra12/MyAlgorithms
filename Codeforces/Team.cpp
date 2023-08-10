6e640cb35783733880914cc8b1a51a5738fce5c5
Shipra Sinha <141357738+sshipra12@users.noreply.github.com>
2023-08-10 10:32:29 +0530

    Team.cpp

---
+++ Codeforces/Team.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int i,surety,n,problems=0,surePeople=0;
+    scanf("%d",&n);
+    while(n--){
+        // for each problem we count how many people out of 3 are sure about it
+        surePeople=0;
+        for(i=0;i<3;i++){
+            scanf("%d",&surety);
+            if(surety==1)
+                surePeople=surePeople+1;
+        }
+        if(surePeople>=2)
+            problems=problems+1;
+    }
+    cout<<problems;
+}
