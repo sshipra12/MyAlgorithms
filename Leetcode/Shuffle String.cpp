5583e3ac1aa6a2c25367411ca685cb5511c1f34c
Shipra Sinha <141357738+sshipra12@users.noreply.github.com>
2023-08-11 08:39:27 +0530

    Create Shuffle String.cpp

---
+++ Leetcode/Shuffle String.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+        int i;
+        string ans=s;
+        //creating a copy of given string and then shuffling
+        for(i=0;i<indices.size();i++){
+            ans[indices[i]]=s[i];
+        }
+        return ans;
+
+      //TODO : shuffling without creating a copy of the given string i.e in place shuffling
+    }
+};
