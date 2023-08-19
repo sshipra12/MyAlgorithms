d0158edcd8426a51baf328ade6d27f02fb0dea9a
Shipra Sinha <141357738+sshipra12@users.noreply.github.com>
2023-08-19 11:23:37 +0530

    Create Find Digits.cpp

---
+++ HackerRank/Find Digits.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'findDigits' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER n as parameter.
+ */
+
+int findDigits(int n) {
+  // get each digit and check if it divides n
+    int num=n,digit,divisors=0;
+    while(num>0){
+        digit=num%10;
+        if(digit!=0&&n%digit==0)
+            divisors++;
+        num=num/10;    
+    }
+    return  divisors;
+}
