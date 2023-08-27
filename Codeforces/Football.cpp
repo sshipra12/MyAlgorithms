69c96df60b16b1e3ee42b6158fa97d0872790f8c
Shipra Sinha <141357738+sshipra12@users.noreply.github.com>
2023-08-27 13:28:02 +0530

    Create Football.cpp

---
+++ Codeforces/Football.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int i,maxConsecutive=0,consecutive=0;
+    string s;
+    cin>>s;
+    for(i=0;i<s.size();i++){
+        if(i==0||s[i]==s[i-1])
+            consecutive++;
+        //breaking conditions
+        if(i==s.size()-1||s[i]!=s[i-1]){
+            if(consecutive>maxConsecutive)
+                maxConsecutive=consecutive;
+            consecutive=1;
+        }
+    }
+    if(maxConsecutive>=7)
+        cout<<"YES";
+    else cout<<"NO";
+}
