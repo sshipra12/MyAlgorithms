044670d9048e4189f77e4cfcc7c4f8049665a5db
Shipra Sinha <141357738+sshipra12@users.noreply.github.com>
2023-08-29 23:54:14 +0530

    Update Check if it is Possible to Split Array.cpp

--- Leetcode/Check if it is Possible to Split Array.cpp
+++ Leetcode/Check if it is Possible to Split Array.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     bool canSplitArray(vector<int>& nums, int m) {
-        //check for atleast one subarray of size 2 has sum greater than or equal to m
-        //if atleast one such subarray is present then we can take that as base
-        //and remove elements one by one from its left side and similarly from its right side
+        // check for atleast one subarray of size 2 has sum greater than or equal to m
+        // if atleast one such subarray is present then we can take that as base
+        // and remove elements one by one from its left side and similarly from its right side
         if(nums.size()==1||nums.size()==2)
             return true;
         int left=0,right=nums.size()-1,tim=nums.size()-2,i,sum=0;
