0c930a120556639ab42c59837bf20dd68bb1b0a3
Shipra Sinha <141357738+sshipra12@users.noreply.github.com>
2023-09-03 14:55:13 +0530

    Update Running Sum of 1d Array.cpp

--- Leetcode/Running Sum of 1d Array.cpp
+++ Leetcode/Running Sum of 1d Array.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
+        // a running sum of an array is runningSum[i] = sum(nums[0]. . .nums[i]).
+        
         int i;
         vector<int> runningSum(nums.size(),0);
         for(i=0;i<nums.size();i++){
