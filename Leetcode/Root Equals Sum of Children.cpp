0f88c2822f4c968ee700977407b16da725d82bc5
Shipra Sinha <141357738+sshipra12@users.noreply.github.com>
2024-03-08 10:26:52 +0530

    Create Root Equals Sum of Children.cpp

---
+++ Leetcode/Root Equals Sum of Children.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool checkTree(TreeNode* root) {
+        if(root->val==nodeVal(root->left)+nodeVal(root->right))
+            return true;
+        return false;    
+    }
+    int nodeVal(TreeNode* node){
+        return node->val;
+    }
+};
