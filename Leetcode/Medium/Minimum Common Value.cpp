9295a460357422c90e272ec6cf73e875a4179f62
Shipra Sinha <141357738+sshipra12@users.noreply.github.com>
2024-03-09 12:32:47 +0530

    Update Minimum Common Value.cpp

--- Leetcode/Medium/Minimum Common Value.cpp
+++ Leetcode/Medium/Minimum Common Value.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        // problem is EASY but due to learning of two sorted vectors iteration simultaneously it is placed in MEDIUM
         // since both arrays are already sorted
         // we just have to move pointers/index of both arrays by comparing their elements with each other
         int ind1=0,ind2=0,common=-1;
