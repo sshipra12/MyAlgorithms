a1cbf0c6ce6eb9ef131e331e31a5437d1659588e
Shipra Sinha <141357738+sshipra12@users.noreply.github.com>
2024-03-09 12:31:13 +0530

    Create Minimum Common Value.cpp

---
+++ Leetcode/Medium/Minimum Common Value.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        // since both arrays are already sorted
+        // we just have to move pointers/index of both arrays by comparing their elements with each other
+        int ind1=0,ind2=0,common=-1;
+        while(ind1<=nums1.size()-1&&ind2<=nums2.size()-1){
+            if(nums1[ind1]==nums2[ind2]){
+                common=nums1[ind1];
+                break;
+            }
+            else if(nums1[ind1]>nums2[ind2]){
+                ind2++;
+            }
+            else{
+                ind1++;
+            }
+        }
+        return common;
+    }
+};
