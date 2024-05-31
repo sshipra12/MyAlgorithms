60d036a4d8a46962811b501eb5f2bfdf119d9386
Shipra Sinha <141357738+sshipra12@users.noreply.github.com>
2024-05-31 22:10:11 +0530

    Update Counting Valleys.cpp

--- HackerRank/Counting Valleys.cpp
+++ HackerRank/Counting Valleys.cpp
@@ -17,6 +17,7 @@ string rtrim(const string &);
 int countingValleys(int steps, string path) {
     int i,altitude=0,countOfValley=0;
     for(i=0;i<=path.size()-1;i++){
+        
         // only condtion for a valley to be found is to check from its ending condition
         // i.e if we are touching alitude level= 0 by going up, then we just travelled through a valley
         if(path[i]=='U'&&altitude==-1)
