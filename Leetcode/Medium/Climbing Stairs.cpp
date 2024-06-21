eca1ab2efbc4c709bdb7839e9629738b4ab3da9b
Shipra Sinha <141357738+sshipra12@users.noreply.github.com>
2024-06-21 09:53:58 +0530

    Update Climbing Stairs.cpp

--- Leetcode/Medium/Climbing Stairs.cpp
+++ Leetcode/Medium/Climbing Stairs.cpp
@@ -26,3 +26,44 @@ public:
         return noOfWaysRecursive(0,n,dp);
     }
 };
+
+// Other solution which gives another view point on the problem
+// It is all about understanding decision tree/recursive tree and which node/state we are calculating
+/*
+class Solution {
+public:
+    int noOfWaysRecursive(int step, int n, int dp[]){
+        if(step==n)
+            return 1;
+        
+        // if we are at second last step we only have 1 choice to jump step+1
+        if(step==n-1){
+            if(dp[step+1]==-1)
+                dp[step+1] = noOfWaysRecursive(step+1,n,dp);
+            return dp[step+1];
+        }
+        
+        // at any other step we have two choices , i.e to jump step+1 or step+2
+        if(dp[step+1]==-1){
+            dp[step+1] = noOfWaysRecursive(step+1,n,dp);
+        }
+
+        if(dp[step+2]==-1){
+            dp[step+2] = noOfWaysRecursive(step+2,n,dp);
+        }
+
+        return dp[step+1]+dp[step+2];
+    }
+
+    int climbStairs(int n) {
+
+        // we use memoization to store the precomputed recursive calls
+        // since each recursive call is dependent on only 1 parameter i.e step number, we create 1D dp array
+        int dp[n+1];
+        for(int i=0;i<=n;i++)
+            dp[i]=-1;
+        
+        return noOfWaysRecursive(0,n,dp);
+    }
+};
+*/
