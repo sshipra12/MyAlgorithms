0c43c0bc3145f9ba60eb5b29d0fc6a2c21ebd103
Shipra Sinha <141357738+sshipra12@users.noreply.github.com>
2024-06-26 22:04:11 +0530

    Update Team.cpp

--- Codeforces/Team.cpp
+++ Codeforces/Team.cpp
@@ -7,6 +7,7 @@ int main()
     int i,surety,n,problems=0,surePeople=0;
     scanf("%d",&n);
     while(n--){
+        
         // for each problem we count how many people out of 3 are sure about it
         surePeople=0;
         for(i=0;i<3;i++){
