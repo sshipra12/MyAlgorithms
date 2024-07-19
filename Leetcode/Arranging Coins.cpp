40e96932e7ee42b743a569a90fc1c2416a953fda
Shipra Sinha <141357738+sshipra12@users.noreply.github.com>
2024-07-19 10:59:05 +0530

    Create Arranging Coins.cpp

---
+++ Leetcode/Arranging Coins.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        // we use of formula of sum of 1st n positive integers
+        // TDOD: write explanation
+        long long int m=0,firstMSum=0;
+        while(firstMSum<n){
+            m++;
+            firstMSum=(m*(m+1))/2;
+        }
+
+        if(firstMSum==n)
+            return m;
+        else return m-1;
+    }
+};
