0abe5232f0451cfa067541834c89e3c8c3c666a2
Shipra Sinha <141357738+sshipra12@users.noreply.github.com>
2024-08-17 11:31:22 +0530

    Create Jump Game.cpp

---
+++ Leetcode/Medium/Jump Game.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool canReachFromIthToTarget(int index, vector<int> &nums){
+        // TDOD: solution in better time complexity
+        // base case: when we have reached target or beyond it
+        if(index==nums.size()-1)
+            return true;
+        else if(index>nums.size()-1)
+            return false;
+
+        // transition: from all the choices we have from current state/subproblem
+        // if from any one of them we can reach target then return true
+        for(int i=1;i<=nums[index];i++){
+            if(canReachFromIthToTarget(index+i,nums))
+                return true;
+        }
+        return false;
+    }
+
+    bool canJump(vector<int>& nums) {
+        // understand state/subproblem meaning and provide required inputs to get answer
+        return canReachFromIthToTarget(0,nums);
+    }
+};
