9e74dd4fade3f351f88f3606ff193ad5de15c0c3
Shipra Sinha <141357738+sshipra12@users.noreply.github.com>
2024-10-09 10:59:08 +0530

    Update Jump Game.cpp

--- Leetcode/Medium/Jump Game.cpp
+++ Leetcode/Medium/Jump Game.cpp
@@ -1,5 +1,30 @@
 class Solution {
 public:
+    // state definition: can we reach from current ith index to target index (i.e n-1 index)
+    bool canReachFromIthToLast(int index, vector<int> &nums){
+        //base case: where transition fails
+        if(index==nums.size()-1)
+            return true;
+        
+        // transition: of all choices from here, check if from anyone of them can we reach target
+        int jumpLength;
+        for(jumpLength=1;jumpLength<=nums[index]&&index+jumpLength<=nums.size()-1;jumpLength++){
+            if(canReachFromIthToLast(index+jumpLength,nums))
+                return true;
+        }
+        return false;
+    }
+
+    bool canJump(vector<int>& nums) {
+        return canReachFromIthToLast(0,nums);
+    }
+};
+
+
+// Old solution
+/*
+class Solution {
+public:
     bool canReachFromIthToTarget(int index, vector<int> &nums){
         
         // TDOD: solution in better time complexity
@@ -24,3 +49,4 @@ public:
         return canReachFromIthToTarget(0,nums);
     }
 };
+*/
