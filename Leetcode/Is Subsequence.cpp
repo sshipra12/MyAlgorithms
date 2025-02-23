d9b021e0ed7fb8d66e8ef672515713a1e84ca85b
Shipra Sinha <141357738+sshipra12@users.noreply.github.com>
2025-02-23 19:38:26 +0530

    Update Is Subsequence.cpp

--- Leetcode/Is Subsequence.cpp
+++ Leetcode/Is Subsequence.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     bool isSubsequence(string s, string t) {
         int i,ind=0,n=t.size();  //NOTE: when t.size()/s.size()=0 , then t.size()-1 or s.size()-1 doesn't = -1
+        
         for(i=0;i<=n-1&&ind<=s.size()-1;i++){
             if(t[i]==s[ind])
                 ind++;
