1d656ca56263d3760d9a3ef46c13d08a97499f58
Shipra Sinha <141357738+sshipra12@users.noreply.github.com>
2025-03-11 21:40:58 +0530

    Update Partition Array According to Given Pivot.cpp

--- Leetcode/Medium/Partition Array According to Given Pivot.cpp
+++ Leetcode/Medium/Partition Array According to Given Pivot.cpp
@@ -1,6 +1,41 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
+        // simple version : using one pointer fix elements less than pivot to the leftmost side
+        // also count elements equal to pivot along the way
+        int i,left=0,n=nums.size(),pivotCount=0;
+        vector<int> ans(n);
+        for(i=0;i<=n-1;i++){
+            // set the less than pivot elements to the leftmost side of the array
+            if(nums[i]<pivot){
+                ans[left]=nums[i];
+                left++;
+            }
+            else if(nums[i]==pivot)
+                pivotCount++;
+        }
+        // set pivot elements to middle
+        while(pivotCount--){
+            ans[left]=pivot;
+            left++;
+        }
+
+        // set elements greater than pivot to the rightmost side
+        for(i=0;i<=n-1;i++){
+            if(nums[i]>pivot){
+                ans[left]=nums[i];
+                left++;
+            }
+        }
+        return ans;
+    }
+};
+
+// OLD Solution:
+/*
+class Solution {
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
         // TODO: A BETTER SOLUTION USING TWO POINTERS
         // simple solution by iterating the array 2 times and using only 1 pointer at a time
         int n=nums.size(),left=0,right=n-1,i;
@@ -29,3 +64,4 @@ public:
         return ans;    
     }
 };
+*/
