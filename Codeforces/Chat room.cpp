1c3c942dd03774e6dae2826c56741c53944148df
Shipra Sinha <141357738+sshipra12@users.noreply.github.com>
2025-03-11 14:50:25 +0530

    Update Chat room.cpp

--- Codeforces/Chat room.cpp
+++ Codeforces/Chat room.cpp
@@ -9,7 +9,6 @@ int main()
     
     // we just have to find if string "hello" occurs in given string s as a subsequence
     // this way we can check whether a Given small string/array occurs as a subsequence of a bigger string/array
-    
     cin>>s;
     for(i=0;i<s.size()&&ind<hello.size();i++){
         if(s[i]==hello[ind])
